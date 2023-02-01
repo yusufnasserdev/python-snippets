4e05bd7db348e3384afbb782a76add7cd53e978c
yusufnasserdev <yusufnassereng@gmail.com>
2023-02-01 21:50:51 +0200

    Added pandas notebook

---
+++ pandas/snippets.py
@@ -0,0 +1,25 @@
+#1. Writing into csv with unnecessary index
+#2. Using column names which include spaces
+#3. Filter dataset like a PRO with QUERY method
+#4.  query strings with(@ symbol) to easily reach variables
+#5. "inplace" method could be removed in future versions, better explicitly overwrite modifications
+#6. better Vectorization instead of iteration
+#7. Vectorization method are preferable than Apply method
+#8. df.copy() method
+#9. chaining formulas is better than creating many intermediate dataframes
+#10. properly set column dtypes
+#11. using Boolean instead of Strings
+#12.  pandas plot method instead of matplotlib import
+#13. pandas str.upper() instead apply and etc
+#14. use data pipeline once instead of repeating many times
+#15. learn proper way of renaming columns
+#16. learn proper way of grouping values
+#17. proper way of complex grouping values
+#18. percent_change or difference now could be implemend with function
+#19. save time and space with large datasets with pickle,parquet,feather formats
+#20. conditional format in pandas(like in Microsoft Excel)
+#21. use suffixes while merging TWO dataframes
+#22. check merging is success with validation 
+#23. wrapping expression so they are readable 
+#24. categorical datatypes use less space
+#25. duplicating columns after concatenating, code snippet
